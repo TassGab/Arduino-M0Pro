ff1d36541c6d0263c38c6046b60c53df15114863
NewBee <31762504+TassGab@users.noreply.github.com>
2017-12-11 17:36:30 +0100

    Add files via upload

---
+++ RTC/Example-rtc-timer/Realy_toggle.ino
@@ -0,0 +1,29 @@
+int Relay=6; //Relay pin
+bool toggle;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(Relay,OUTPUT);
+digitalWrite(Relay,LOW);
+Serial.begin(38400);
+Serial.println("Hello");
+toggle=false;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+if(toggle) 
+{
+   digitalWrite(Relay,HIGH);
+  Serial.println("OFf");
+  toggle=false;
+}
+else 
+{
+  toggle=true;
+  Serial.println("ON");
+  pinMode(Relay,OUTPUT);
+  digitalWrite(Relay,LOW);
+}
+delay(5000);
+}
