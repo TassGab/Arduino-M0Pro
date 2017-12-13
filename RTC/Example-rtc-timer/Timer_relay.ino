34cbb380b6f5424f9048fe0d0d2ce21671918544
NewBee <31762504+TassGab@users.noreply.github.com>
2017-12-13 08:39:48 +0100

    Add files via upload
    Timer set for Christmas tree light timing. Switch on at 19:00 and Switch off at 23:00

---
+++ RTC/Example-rtc-timer/Timer_relay.ino
@@ -0,0 +1,45 @@
+#include <RTCZero.h>
+int Relay = 6; //Relay pin
+bool toggle;
+RTCZero rtc;
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, LOW);
+  Serial.begin(38400);
+  Serial.println("RTC test");
+  rtc.begin();
+  rtc.setTime(17, 29, 50);
+  rtc.setDate(11, 12, 17);
+  rtc.setAlarmTime(17, 30, 0);
+  rtc.enableAlarm(rtc.MATCH_SS);
+  rtc.attachInterrupt(alarmMatch);
+  toggle = true;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Min=");
+  Serial.println(rtc.getMinutes());
+  delay(36000);
+}
+
+void alarmMatch()
+{
+  if (toggle)
+  {
+    digitalWrite(Relay, HIGH);
+    Serial.println("OFF");
+    toggle = false;
+  }
+  else
+  {
+    toggle = true;
+    Serial.println("ON");
+    pinMode(Relay, OUTPUT);
+    digitalWrite(Relay, LOW);
+  }
+  rtc.setAlarmSeconds(10);
+}
+
